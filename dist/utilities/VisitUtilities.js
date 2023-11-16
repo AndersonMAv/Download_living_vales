3c185d080db11fb0027d1a8236fbc2c51c61f404
anm <anm@interact.com.br>
2023-11-16 10:30:14 -0300

    another download

---
+++ dist/utilities/VisitUtilities.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.codMunicipios = void 0;
+exports.codMunicipios = [
+    {
+        codigo: 4322707,
+        municipio: "Vera Cruz"
+    }
+];
+//# sourceMappingURL=VisitUtilities.js.map
